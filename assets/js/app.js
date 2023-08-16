f274175bbcfda75005f812363415184f663a7549
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-15 23:06:12 -0400

    Agregar tarea y renderizar tarea, estos métodos permiten crear un nuevo objeto con un uuid generado con los métodos Math.random, toString y substring, se creó el método capitalize para limpiar los textos y se renderiza el TODO recorriendo los valores del objeto tareas y agregando el template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,47 @@
 // ------------------- Variables
 const formulario = document.querySelector('#formulario');
-const tarea = document.querySelector('#tarea');
 const listaTareas = document.querySelector('#lista-tareas');
 const listaTareasTemplate = document.querySelector('#lista-tareas-template').content;
 const fragmentlistaTareasTemplate = document.createDocumentFragment();
 
+const tarea = document.querySelector('#tarea');
+const tareas = {};
+
 // ------------------- Funciones
+// 
+// ------------------- Crear una nueva tarea en el objeto tareas
 const agregarTarea = () => {
-    const clonListaTareasTemplate = listaTareasTemplate.cloneNode(true);
+    const nuevaTarea = {
+        // ------------------- Devolver un número aleatorio entre 0-1 convertirlo a alfanumérico y elimnar la parte entera y el .
+        id: Math.random().toString(36).substring(2),
+        title: capitalize(tarea.value.trim())
+    };
 
-    clonListaTareasTemplate.querySelector('.alert p').textContent = capitalize(tarea.value.trim());
+    // ------------------- Crear la nueva tarea en la lista
+    tareas[nuevaTarea.id] = { ...nuevaTarea };
 
-    fragmentlistaTareasTemplate.appendChild(clonListaTareasTemplate);
-    listaTareas.appendChild(fragmentlistaTareasTemplate);
-
+    renderizarTareas();
     formulario.reset();
 };
 
-const capitalize = (word) => `${word.charAt(0).toUpperCase()}${word.toLowerCase().slice(1)}`;
+// ------------------- Renderizar el listado de tareas
+const renderizarTareas = () => {
+    listaTareas.textContent = '';
+
+    Object.values(tareas).forEach((tarea) => {
+        const clonListaTareasTemplate = listaTareasTemplate.cloneNode(true);
+
+        clonListaTareasTemplate.querySelector('.alert p').id = tareas[tarea.id].id;
+        clonListaTareasTemplate.querySelector('.alert p').textContent = tareas[tarea.id].title;
+
+        fragmentlistaTareasTemplate.appendChild(clonListaTareasTemplate);
+    });
+
+    listaTareas.appendChild(fragmentlistaTareasTemplate);
+};
+
+// ------------------- Formatear a capitalize la tarea
+const capitalize = (word) => `${word.charAt(0).toUpperCase()}${word.toLowerCase().substring(1)}`;
 
 // ------------------- Delegación de eventos
 //
